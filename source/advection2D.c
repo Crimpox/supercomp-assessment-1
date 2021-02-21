d4fe981c62b2191cbe1c6793337a759e1d893399
Leon Cresdee <leon@cresdee.com>
2021-02-21 21:05:12 +0000

    Update advection2D.c

--- source/advection2D.c
+++ source/advection2D.c
@@ -180,7 +180,8 @@ int main(){
         if (j <= roughlen){
           velx = 0.0;
         } else{
-          velx = (fricvel/k) * log((float)j/roughlen);
+          velx = (fricvel/k) * log(j/roughlen);
+          printf("velx: %f\n", velx);
         }
 
 	      dudt[i][j] = -velx * (u[i][j] - u[i-1][j]) / dx
